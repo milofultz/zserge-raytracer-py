259fec6de9662074ef6162974e6e1121829d1a3e
Milo Fultz <milopfultz@gmail.com>
2021-12-19 16:50:22 +0700

    Make Sphere class

---
+++ Sphere.py
@@ -0,0 +1,24 @@
+import math
+import Vec
+
+
+class Sphere:
+    def __init__(self, center: Vec, radius: float, color: float):
+        self.center = center
+        self.radius = radius
+        self.color = color # 0 = black, 1 = white
+
+    def intersect(self, origin: Vec, direction: Vec) -> float:
+        p = origin - self.center
+        a = direction % direction
+        b = 2 * (p % direction)
+        c = (p % p) - (self.radius * self.radius)
+        d = b * b - 4 * (a * c)
+        if d < 0:
+            return float('nan')
+        sqd = math.sqrt(d)
+        distance = (-b - sqd) / (2 * a)
+        if distance > .1:
+            return distance
+        else:
+            return float('nan')
